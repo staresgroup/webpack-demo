d2dc877033b173da715733adeffba241a636344b
staresgroup <yingpin_c@163.com>
2021-04-02 21:52:23 +0800

    feat: load css

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,13 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'development'
+  mode: 'development',
+  module: {
+      rules: [
+          {
+              test: /\.css$/i,
+              use: ['style-loader', 'css-loader']  
+          }
+      ]
+  }
 };
